0b5cde7aeda8d2afcacab678be570163414e54cc
Jerry Chapman <raptor90se@msn.com>
2019-11-22 15:17:50 -0700

    added blink logic

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 
 void setup() {
   // put your setup code here, to run once:
-  // this is a test
+  Serial.begin(9600);
+  pinMode(2,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("ON");
+  digitalWrite(LED_GPIO, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_GPIO, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("OFF");
+  delay(1000); 
 }
